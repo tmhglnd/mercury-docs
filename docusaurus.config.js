b0667b25da65e7d8f11aea17e5998f10435712b9
tmhglnd <timohoogland@gmail.com>
2024-05-16 00:56:15 +0200

    initial commit for new updated mercury docs

---
+++ docusaurus.config.js
@@ -0,0 +1,168 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Mercury',
+  tagline: 'A minimal and human-readable language for the live coding of algorithmic electronic music performances',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'tmhglnd', // Usually your GitHub org/user name.
+  projectName: 'mercury', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/docs',
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 4,
+      },
+      // Replace with your project's social card
+      // image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Mercury',
+        logo: {
+          alt: 'Mercury Logo',
+          // src: 'img/logo.svg',
+          src: 'img/mercury.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/tmhglnd/mercury-playground',
+            label: 'Github',
+            position: 'right',
+          },
+          // {
+          //   href: 'https://mercury.timohoogland.com',
+          //   label: 'Playground',
+          //   position: 'right',
+          // },
+          // {
+          //   href: 'https://github.com/tmhglnd/mercury-playground',
+          //   label: 'Mercury Playground',
+          //   position: 'right',
+          // },
+          // {
+          //   href: 'https://github.com/tmhglnd/mercury',
+          //   label: 'Mercury4Max',
+          //   position: 'right',
+          // },
+          // {
+          //   href: 'https://github.com/tmhglnd/mercury-engine',
+          //   label: 'Mercury Engine',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Docs',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              // {
+              //   label: 'Stack Overflow',
+              //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              // },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/vt59NYU',
+              },
+              {
+                label: 'Mastodon',
+                href: 'https://social.toplap.org/@mercury',
+              },
+            ],
+          },
+          // {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: '/blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/facebook/docusaurus',
+          //     },
+          //   ],
+          // },
+        ],
+        copyright: `Copyright (c) ${new Date().getFullYear()} Mercury Live Coding Environment by Timo Hoogland. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
