12a833a9315e033cba710e0e7b494c0dfd02b019
tmhglnd <timohoogland@gmail.com>
2026-03-14 11:21:42 +0100

    updated url

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://mercury.timohoogland.com">
+            to="https://mercury-playground.cc">
             Start Coding
           </Link>
           &nbsp;&nbsp;&nbsp;&nbsp;
